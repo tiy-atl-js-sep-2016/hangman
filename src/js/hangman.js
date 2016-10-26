445e4856a0892ae6a78d53d72f1c0a88ba95af3d
Brit Butler <redline6561@gmail.com>
2016-10-26 11:09:58 -0400

    Render a game board with javascript. leh go.

--- src/js/hangman.js
+++ src/js/hangman.js
@@ -1,14 +1,28 @@
+var ALPHABET = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+  'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+  'w', 'x', 'y', 'z'
+];
+
 function Game (options) {
   options = options || {};
   this.turnsLeft = options.turnsLeft || 6;
   this.dictionary = options.dictionary;
   this.guesses = [];
   this.word = this.dictionary.getWord();
+  this.alphabet = ALPHABET;
+};
+
+Game.prototype.renderAlphabet = function () {
+  var alphabetHTML = this.alphabet.map(function (letter) {
+    return `<span class="letter">${letter.toUpperCase()}</span>`;
+  });
+  return alphabetHTML.join("");
 };
 
 Game.prototype.makeGuess = function (guess) {
   this.guesses.push(guess);
-  if (!this.word.includes("o")) {
+  if (!this.word.includes(guess)) {
     this.turnsLeft -= 1;
   }
 };
--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,12 @@ import $ from 'jquery';
 import _ from 'lodash';
 
 import { Game } from "./hangman";
+import { ArrayDictionary as Dictionary } from "./dictionary";
+
+var dictionary = new Dictionary();
+var game = new Game({ dictionary: dictionary });
+
+var alphabetHtml = game.renderAlphabet();
+$(".alphabet").html(alphabetHtml);
+
+console.log(game);
